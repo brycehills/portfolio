e3705aaf79808244ef2a7a4023bd4c27d8a65d9f
brycehills <hillsbryce@yahoo.com>
2023-08-18 14:56:47 -0700

    new init

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const Home: NextPage = () => {
+
+  return(
+    <div>
+      <Head>
+        <title>Bryce Portfolio</title>
+      </Head>
+      <h1>lets get dank</h1>
+    </div>
+  )
+
+
+}
+
+export default Home;
